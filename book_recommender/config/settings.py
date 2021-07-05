804e0629967b0a4b36c736754a5dcf7fd183b4aa
makerseok <maker7788@gmail.com>
2021-07-05 18:27:58 +0900

    initialize django project

---
+++ book_recommender/config/settings.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a25359dc3ff4fa15c292a45e3ade2a3584caf8b8a2d4ad0f23b7a1cd045d7d77
+size 3298
---
+++ book_recommender/config/urls.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5a52bf00f0780a9d2befa4298537c581e9d4b926372f0c4980bfea1586447e06
+size 854
