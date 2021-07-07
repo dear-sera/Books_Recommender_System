f6d27e06e91e83b8c3a06b5c4970f18c38879427
makerseok <maker7788@gmail.com>
2021-07-07 20:36:53 +0900

    make base layout

---
+++ web/frontend/src/components/layout/Footer.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8849b8552c628383a1f1d88a233bd5586380eceb9057acefb8a5bab02c0c2ae0
+size 128
---
+++ web/frontend/src/components/layout/Header.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c0f2c3828559855e6f3c048413d0b2a202d8088cccb10518db1a5da0314d20d
+size 486
