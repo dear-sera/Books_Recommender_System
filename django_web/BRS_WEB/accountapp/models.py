0610a802c08fb7124cf5651a1a59da8e6e00f658
5kdk <tylerk1121@gmail.com>
2021-07-08 09:37:12 +0900

    transition add

--- django_web/BRS_WEB/accountapp/models.py
+++ django_web/BRS_WEB/accountapp/models.py
@@ -3,4 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-class HelloWorld(models.Moedel):
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
+    
